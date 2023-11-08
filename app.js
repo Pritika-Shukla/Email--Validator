cdf5a50b9634f2648e215356fb7a50b17adcd31d
Priitka Shukla <Pritika@pritikashukla21@gmail.com>
2023-11-08 18:34:31 +0530

    third commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let result={
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "pritikashukla21",
+    "email": "pritikashukla21@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+  }
+  let str=``
+  for(key of Object.keys(result)){
+    str=str+ `<div>${key}: ${result[key]}</div>`
+  }
+  console.log(str)
+results.innerHTML=str
