b7ea7405d0f6322a344e7cd5888e5f0be03e0108
Pritika-Shukla <pritikashukla21@gmail.com>
2023-11-11 10:09:46 +0530

    added features

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 let result={
-    "tag": "",
-    "free": true,
-    "role": false,
-    "user": "pritikashukla21",
-    "email": "pritikashukla21@gmail.com",
-    "score": 0.64,
-    "state": "deliverable",
-    "domain": "gmail.com",
-    "reason": "valid_mailbox",
-    "mx_found": true,
-    "catch_all": null,
-    "disposable": false,
-    "smtp_check": true,
-    "did_you_mean": "",
-    "format_valid": true
-  }
+  "tag": "",
+  "free": true,
+  "role": false,
+  "user": "pritikashukla21",
+  "email": "pritikashukla21@gmail.com",
+  "score": 0.64,
+  "state": "deliverable",
+  "domain": "gmail.com",
+  "reason": "valid_mailbox",
+  "mx_found": true,
+  "catch_all": null,
+  "disposable": false,
+  "smtp_check": true,
+  "did_you_mean": "",
+  "format_valid": true
+}
+Sbtn.addEventListener("click", async (e)=>{
+  e.preventDefault()
+  console.log("clicked")
+  results.innerHTML= `<img width=72px src="Spinner-1s-200px.gif" alt="loader" >`
+let key="ema_live_6JRN7Vvclvz4ZnsMQtiQBnI4iiBMXHPEqnHOs8VT"
+let email=document.getElementById("email").value
+let url=`https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+let res=await fetch(url)
+let result=await res.json()
   let str=``
   for(key of Object.keys(result)){
+    if(result[key] !=="" &&result[key] !==" "){
     str=str+ `<div>${key}: ${result[key]}</div>`
   }
+  }
   console.log(str)
-results.innerHTML=str
+   results.innerHTML=str
+})
